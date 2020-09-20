9e234fca2fc1633b89e8c562708c9e4cbb8e9e04
Viktor <nuzhdinvitja@gmail.com>
2020-09-20 12:16:35 +0300

    Replaced loop in extractWords to regex (#1)
    * replaced loop in extractWords to regex
    
    * made regex shorter and removed comma
    
    * added latin alphabet to the regex

--- src/utils/extractWords.js
+++ src/utils/extractWords.js
@@ -1,50 +1,10 @@
-const cyrillicStr = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя-'
-const cyrillic = cyrillicStr
-    .split('')
-    .reduce((obj, char, i) => {
-        obj[char] = i
-        return obj
-    }, {})
 
 function extractWords(message) {
     if(!message) {
         return []
     }
 
-    const words = []
-
-    let inWord = message[0] in cyrillic
-    let currentWord = ''
-
-    for(let i = 0; i < message.length; i += 1) {
-        const char = message[i]
-
-        if(inWord) {
-            if(char in cyrillic) {
-                currentWord += char
-            } else {
-                inWord = false
-                words.push( currentWord )
-                currentWord = ''
-
-                // reiterate
-                i -= 1
-            }
-        } else {
-            if(char in cyrillic) {
-                inWord = true
-
-                // reiterate
-                i -= 1
-            }
-        }
-    }
-
-    if(currentWord) {
-        words.push( currentWord )
-    }
-
-    return words
+    return message.match(/([а-яёa-z])+/gi) ?? [];
 }
 
 module.exports = extractWords
